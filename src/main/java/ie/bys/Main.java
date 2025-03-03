32cdf7c0947a275adcce78922555f7100cbc083b
goga37 <bystrushkin.igor@gmail.com>
2025-03-04 00:48:18 +0300

    Update Main.java

--- src/main/java/ie/bys/Main.java
+++ src/main/java/ie/bys/Main.java
@@ -7,6 +7,6 @@ public class Main {
         this.jjhv = jjhv;
     }
     public static void main(String[] args) {
-        System.out.println("Hello, hbhgvjyb!");
+        System.out.println("Hello, hbhgvl,,,,,,,,,,,,,,,,,,,,,,,,,,,okijy7ftghujiokp9o8hujyb!");
     }
 }
