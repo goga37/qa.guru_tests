3150ecef511d352752f03a9fc27d31e12ac03309
goga37 <bystrushkin.igor@gmail.com>
2025-03-04 10:09:07 +0300

    Update Main.java

--- src/main/java/ie/bys/Main.java
+++ src/main/java/ie/bys/Main.java
@@ -12,6 +12,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello, 2313213213");
+        System.out.println("Hello, 3213213213");
     }
 }
