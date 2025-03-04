8fbff84835a9ee42a4fe337d6fa9f9e416a92e31
goga37 <bystrushkin.igor@gmail.com>
2025-03-04 10:04:43 +0300

    Update Main.java

--- src/main/java/ie/bys/Main.java
+++ src/main/java/ie/bys/Main.java
@@ -3,8 +3,8 @@ package ie.bys;
 public class Main {
     String jjhv = "grgergr";
 
-    public Main(String jjhv) {
-        this.jjhv = jjhv;
+    public Main(String fewfef) {
+        this.fewfef = fewfef;
     }
     public static void main(String[] args) {
         System.out.println("Hello, hbhgvl,,,,,,,,,,,,,,,,,njknnjnjnj,,,,,,,,,,okijy7ftghujiokp9o8hujyb!");
